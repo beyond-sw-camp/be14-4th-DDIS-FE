92cc01a1061640ec87dbcc210e4c7c13da63a2ff
LAPTOP-4PKBB9GJ\Playdata <tommy8969@naver.com>
2025-04-29 14:11:12 +0900

    feat: 관리자 헤더 구현

--- src/pages/ReportPage.vue
+++ src/pages/ReportPage.vue
@@ -1,11 +1,14 @@
 <template>
+    <header>
+        <AdminHeader/>
+    </header>
     <div>
 
     </div>
 </template>
 
 <script setup>
-
+    import AdminHeader from '@/components/header/AdminHeader.vue';
 </script>
 
 <style scoped>
